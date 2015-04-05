8b1965c47e3815b788a48c55c5ac7cb0197d1561
Jacek Kościesza <jacek.kosciesza@gmail.com>
2015-04-05 11:06:16 -0700

    Added MVC to the request pipeline

--- WorkMarketingNet.Web/Startup.cs
+++ WorkMarketingNet.Web/Startup.cs
@@ -10,10 +10,23 @@ namespace WorkMarketingNet.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+			// Add MVC services to the services container.
+			services.AddMvc();
+		}
 
         public void Configure(IApplicationBuilder app)
         {
-        }
+			// Add static files to the request pipeline.
+			app.UseStaticFiles();
+
+			// Add MVC to the request pipeline.
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "Home", action = "Index" });
+			});
+		}
     }
 }
