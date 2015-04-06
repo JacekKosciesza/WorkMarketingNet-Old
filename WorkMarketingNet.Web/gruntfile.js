04b976ff6a58f6b2636aa0068a495bdc56d56d3f
Jacek Kościesza <jacek.kosciesza@gmail.com>
2015-04-06 02:16:17 -0700

    Polymer "Hello, World!"

--- WorkMarketingNet.Web/gruntfile.js
+++ WorkMarketingNet.Web/gruntfile.js
@@ -1,4 +1,4 @@
-/// <binding AfterBuild='less' />
+/// <binding AfterBuild='less, copy' />
 /*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
@@ -44,11 +44,20 @@ module.exports = function (grunt) {
 					dest: 'wwwroot/images/'        // Destination path prefix
 				}]
 			}
-		}
+		},
+		copy: {
+			main: {
+				files: [
+				  // includes files within path
+				  { expand: true, flatten: true, src: ['assets/polymer/*'], dest: 'wwwroot/polymer/', filter: 'isFile' }
+				],
+			},
+		},
 	});
 	
 	grunt.loadNpmTasks("grunt-bower-task");
 	grunt.loadNpmTasks('grunt-newer');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks("grunt-contrib-less");
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
